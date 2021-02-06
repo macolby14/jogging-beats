54dddb3a409421a61a2e653f124f62ac7d463be1
Mark Colby <macolby14@gmail.com>
2021-02-06 01:51:59 -1000

    add api call to get spotify access token for client

---
+++ pages/api/hello.tsx
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const results = await fetch("https://accounts.spotify.com/api/token", {
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+      ).toString("base64")}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+  }).then((response) => response.json());
+
+  res.status(200).json(results);
+};
