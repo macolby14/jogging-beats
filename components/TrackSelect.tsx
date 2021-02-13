9f7af26f64566f1d0504f57638e8584126f44b82
Mark Colby <macolby14@gmail.com>
2021-02-13 08:09:25 -1000

    add icon for add/remove tracks

--- components/TrackSelect.tsx
+++ components/TrackSelect.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
+import styled from "styled-components";
 
 interface TrackSelectProps {
   className?: string;
@@ -8,15 +9,37 @@ interface TrackSelectProps {
   selectHandler: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
+const IconStyle = styled.img`
+  cursor: pointer;
+`;
+
 export function TrackSelect({
   className = "",
   selected,
   selectHandler,
 }: TrackSelectProps) {
+  let selectIcon;
+
+  if (selected) {
+    selectIcon = (
+      <IconStyle
+        src="/remove_circle_outline-24px.svg"
+        alt="Add Track to Playlist"
+      />
+    );
+  } else {
+    selectIcon = (
+      <IconStyle
+        src="/add_circle_outline-24px.svg"
+        alt="Add Track to Playlist"
+      />
+    );
+  }
+
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div className={className} onClick={selectHandler}>{`${Boolean(
-      selected
-    )}`}</div>
+    <div className={className} onClick={selectHandler}>
+      {selectIcon}
+    </div>
   );
 }
