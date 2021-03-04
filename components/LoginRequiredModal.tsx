559787193f5cf7483e0ca0743acbd925ffe14878
Mark Colby <macolby14@gmail.com>
2021-03-03 21:41:37 -1000

    add success checkmark to post login

--- components/LoginRequiredModal.tsx
+++ components/LoginRequiredModal.tsx
@@ -1,8 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Modal from "react-modal";
+import styled from "styled-components";
 import { SpotifyAuthPop } from "./AuthPop/SpotifyAuthPop";
 import { ImplicitAuthContext } from "./context/ImplicitAuthProvider";
 import { Heading } from "./Heading";
+import { Spacer } from "./Spacer";
 
 Modal.setAppElement("#__next");
 
@@ -19,6 +21,81 @@ const customStyles = {
   },
 };
 
+const LoginCompleteStyle = styled.div`
+  svg {
+    width: 100px;
+    display: block;
+    margin: 40px auto 0;
+  }
+
+  .path {
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 0;
+    &.circle {
+      -webkit-animation: dash 0.9s ease-in-out;
+      animation: dash 0.9s ease-in-out;
+    }
+    &.line {
+      stroke-dashoffset: 1000;
+      -webkit-animation: dash 0.9s 0.35s ease-in-out forwards;
+      animation: dash 0.9s 0.35s ease-in-out forwards;
+    }
+    &.check {
+      stroke-dashoffset: -100;
+      -webkit-animation: dash-check 0.9s 0.35s ease-in-out forwards;
+      animation: dash-check 0.9s 0.35s ease-in-out forwards;
+    }
+  }
+
+  p {
+    text-align: center;
+    margin: 20px 0 60px;
+    font-size: 1.25em;
+    &.success {
+      color: #73af55;
+    }
+    &.error {
+      color: #d06079;
+    }
+  }
+
+  @-webkit-keyframes dash {
+    0% {
+      stroke-dashoffset: 1000;
+    }
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dashoffset: 1000;
+    }
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @-webkit-keyframes dash-check {
+    0% {
+      stroke-dashoffset: -100;
+    }
+    100% {
+      stroke-dashoffset: 900;
+    }
+  }
+
+  @keyframes dash-check {
+    0% {
+      stroke-dashoffset: -100;
+    }
+    100% {
+      stroke-dashoffset: 900;
+    }
+  }
+`;
+
 interface Props {
   isOpen: boolean;
   setIsOpen: Function;
@@ -26,6 +103,56 @@ interface Props {
 
 export function LoginRequiredModal({ isOpen, setIsOpen }: Props) {
   const { setUserToken } = useContext(ImplicitAuthContext);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const loginDisplay = (
+    <>
+      <Heading level={4}>You must login to Spotfiy to add a Playlist</Heading>
+      <SpotifyAuthPop
+        onCode={async (code: string) => {
+          setUserToken(code);
+          setLoggedIn(true);
+          setTimeout(() => {
+            setIsOpen(false);
+          }, 5000);
+        }}
+      />
+    </>
+  );
+
+  // Source: https://codepen.io/elevaunt/pen/VvKdVa
+  const loginCompleteDisplay = (
+    <LoginCompleteStyle>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 130.2 130.2"
+      >
+        <circle
+          className="path circle"
+          fill="none"
+          stroke="#73AF55"
+          strokeWidth="6"
+          strokeMiterlimit="10"
+          cx="65.1"
+          cy="65.1"
+          r="62.1"
+        />
+        <polyline
+          className="path check"
+          fill="none"
+          stroke="#73AF55"
+          strokeWidth="6"
+          strokeLinecap="round"
+          strokeMiterlimit="10"
+          points="100.2,40.2 51.5,88.8 29.8,67.5 "
+        />
+      </svg>
+      <p className="success">You successfully logged in with Spotify</p>
+      <Spacer size={10} />
+      <p className="success">You can now create your playlist!</p>
+    </LoginCompleteStyle>
+  );
 
   return (
     <Modal
@@ -37,13 +164,7 @@ export function LoginRequiredModal({ isOpen, setIsOpen }: Props) {
       style={customStyles}
       contentLabel="Login Required Modal"
     >
-      <Heading level={4}>You must login to Spotfiy to add a Playlist</Heading>
-      <SpotifyAuthPop
-        onCode={(code: string) => {
-          setUserToken(code);
-          setIsOpen(false);
-        }}
-      />
+      {loggedIn ? loginCompleteDisplay : loginDisplay}
     </Modal>
   );
 }
