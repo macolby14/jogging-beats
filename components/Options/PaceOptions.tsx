e5026d982da61db6c513a742704d2b3ee0c13a2c
Mark Colby <macolby14@gmail.com>
2021-03-10 14:21:44 -1000

    add change inputs based on pace option selected

--- components/Options/PaceOptions.tsx
+++ components/Options/PaceOptions.tsx
@@ -7,10 +7,16 @@ const PaceOptionsStyle = styled.div`
   grid-template-columns: 1fr 1fr 50px;
   column-gap: 16px;
   row-gap: 16px;
+  font-size: var(--text-size-4);
 
+  input,
   select {
     font-family: inherit;
     font-size: inherit;
+  }
+
+  select {
+    width: auto;
   }
 
   label {
@@ -19,6 +25,18 @@ const PaceOptionsStyle = styled.div`
     justify-content: flex-end;
   }
 `;
+
+const FlexRowStyle = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+
+  input {
+    text-align: right;
+  }
+`;
+
+type OptionType = "RUNNING" | "CYCLING" | "BPM" | "INTENSITY";
 
 interface PaceOptionsProps {
   bpm: number;
@@ -33,22 +51,67 @@ export function PaceOptions({
   targetDuration,
   setTargetDuration,
 }: PaceOptionsProps) {
+  const [selectedOption, setSelectedOption] = useState<OptionType>("RUNNING");
+
+  function optionChangeHandler(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedOption(e.target.value as OptionType);
+  }
+
+  function paceInputOptions() {
+    switch (selectedOption) {
+      case "CYCLING":
+      case "RUNNING":
+        return (
+          <FlexRowStyle>
+            <FlexRowStyle>
+              <input type="text" />
+              min
+            </FlexRowStyle>
+            <FlexRowStyle>
+              <input type="text" />
+              sec
+            </FlexRowStyle>
+          </FlexRowStyle>
+        );
+      case "INTENSITY":
+        return (
+          <select name="intensity" id="intensity">
+            <option value="LOW">Relaxed</option>
+            <option value="MODERATE_LOW">Moderate Low</option>
+            <option value="MODERATE">Moderate</option>
+            <option value="MODERATE_HIGH">Moderate High</option>
+            <option value="HIGH">High</option>
+          </select>
+        );
+      case "BPM":
+        return (
+          <input
+            type="number"
+            name="pace"
+            value={bpm}
+            onChange={(e) => setBpm(parseInt(e.target.value, 10))}
+          />
+        );
+      default:
+        return <div>Something went wrong</div>;
+    }
+  }
+
   return (
     <PaceOptionsStyle>
-      <label htmlFor="bpm">
-        <select name="paceOptions" id="paceOptions">
-          <option>Running Pace</option>
-          <option>Cycling Pace</option>
-          <option>Beats Per Minute</option>
-          <option>Workout Intensity</option>
+      <label htmlFor="pace">
+        <select
+          name="paceOptions"
+          id="paceOptions"
+          onChange={optionChangeHandler}
+        >
+          <option value="RUNNING">Running Pace</option>
+          <option value="CYCLING">Cycling Pace</option>
+          <option value="BPM">Beats Per Minute</option>
+          <option value="INTENSITY">Workout Intensity</option>
         </select>
       </label>
-      <input
-        type="number"
-        name="bpm"
-        value={bpm}
-        onChange={(e) => setBpm(parseInt(e.target.value, 10))}
-      />
+      {paceInputOptions()}
       <Spacer size={50} />
       <label htmlFor="workoutTime">Workout Time (min)</label>
       <input
