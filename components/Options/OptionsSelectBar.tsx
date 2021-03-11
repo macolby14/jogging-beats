d60700ed4347806bc903fd020c02d369ca578ee4
Mark Colby <macolby14@gmail.com>
2021-03-10 22:40:16 -1000

    style add border to options to make them more visible

--- components/Options/OptionsSelectBar.tsx
+++ components/Options/OptionsSelectBar.tsx
@@ -9,6 +9,7 @@ const OptionsSelectBarStyle = styled.ul`
   flex-direction: row;
   justify-content: center;
   font-size: var(--text-size-3);
+  box-shadow: 1px 2px 2px #df6430;
 
   li {
     display: inline;
@@ -17,8 +18,12 @@ const OptionsSelectBarStyle = styled.ul`
     color: var(--white);
     font: inherit;
     cursor: pointer;
+    border: 1px solid rgba(56, 26, 13, 0.2);
+    border-left: none;
   }
-
+  li:first-child {
+    border-left: 1px solid rgba(56, 26, 13, 0.2);
+  }
   li:hover {
     background-color: var(--light);
   }
