5f8c2864772a0988bfea63246bdec10e59b009f6
Mark Colby <macolby14@gmail.com>
2021-03-13 15:28:41 -1000

    style change use inline svg for refresh songs

--- components/PlaylistCreationMenu.tsx
+++ components/PlaylistCreationMenu.tsx
@@ -3,6 +3,7 @@ import { durationFormat } from "../utils/durationFormat";
 import { Heading } from "./Heading";
 import { PlaylistCreationButton } from "./PlaylistCreationButton";
 import { Spacer } from "./Spacer";
+import { Tooltip } from "./Tooltip";
 import { TrackData } from "./Track";
 
 interface PlaylistCreationButtonProps {
@@ -23,6 +24,15 @@ const PlaylistCreationMenuStyle = styled.div`
   top: 0px;
   background-color: rgba(255, 255, 255, 0.98);
   z-index: 1;
+`;
+
+const RefreshStyle = styled.svg`
+  fill: green;
+  cursor: pointer;
+
+  :hover {
+    fill: black;
+  }
 `;
 
 export function PlaylistCreationMenu({
@@ -54,9 +64,17 @@ export function PlaylistCreationMenu({
         title={playlistTitle}
         description={playlistDescription}
       />
-      <div>
-        <img src="/cached-24px.svg" alt="Get new Songs" width="100px" />
-      </div>
+      <Tooltip direction="top" text="Change out the unselected songs">
+        <RefreshStyle
+          xmlns="http://www.w3.org/2000/svg"
+          height="100"
+          viewBox="0 0 24 24"
+          width="100"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z" />
+        </RefreshStyle>
+      </Tooltip>
     </PlaylistCreationMenuStyle>
   );
 }
