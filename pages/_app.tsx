c5dfbe1fdb720fb5d5d59418d926e3a159edfc3f
Mark Colby <macolby14@gmail.com>
2021-03-14 15:09:25 -1000

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Jogging Beats</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;0,700;1,400;1,700&display=swap"
