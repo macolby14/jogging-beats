e48944f3e90c79a4c5d6845b4e191f4bbebee0e0
Mark Colby <macolby14@gmail.com>
2021-03-15 17:30:54 -1000

    fix - fix home page link button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
-import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
 import { HeroBox } from "../components/HeroBox";
 import { Spacer } from "../components/Spacer";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <HeroBox />
       <Spacer size={16} />
-      <button type="button" style={{ margin: "auto" }}>
-        <Link href="/search">
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a>Get Started</a>
-        </Link>
+      <button
+        type="button"
+        style={{ margin: "auto" }}
+        onClick={() => {
+          router.push("/search");
+        }}
+      >
+        Get Started
       </button>
     </>
   );
