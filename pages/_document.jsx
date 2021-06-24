d1b2507b1c4036ac8b15d9be9536a0ce37000a5d
Mark Colby <macolby14@gmail.com>
2021-06-24 10:24:11 -1000

    add meta and open graph tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -32,7 +32,45 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="UTF-8" />
+          {/* Viewport tag in _app.tsx https://err.sh/next.js/no-document-viewport-meta */}
+          {/* Default title in _app.tsx https://nextjs.org/docs/messages/no-document-title */}
+          <meta
+            name="description"
+            content="A web app that allows users to generate playlists for a target running tempo and add them to their Spotify account.  Sync your workout to a tempo to go faster."
+          />
+          <meta
+            property="og:title"
+            content="Jogging Beats - A workout music playlist generation site"
+            key="title"
+          />
+          <meta
+            property="og:description"
+            content="A web app that allows users to generate playlists for a target running tempo and add them to their Spotify account.  Sync your workout to a tempo to go faster."
+          />
+          <meta
+            name="image"
+            property="og:image"
+            content="https://joggingbeats.com/home_og_image.jpg"
+          />
+          <meta
+            property="og:image:secure_url"
+            content="https://joggingbeats.com/home_og_image.jpg"
+          />
+          <meta property="og:image:width" content="1337" />
+          <meta property="og:image:height" content="878" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://joggingbeats.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta name="theme-color" content="#ff860e" />
+          <link rel="icon" href="/favicon.svg" />
+          <link rel="mask-icon" href="/favicon.svg" color="#ff860e" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="canonical" href="https://joggingbeats.com" />
+        </Head>
         <body>
           <Main />
           <NextScript />
